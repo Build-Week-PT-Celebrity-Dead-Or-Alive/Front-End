589bea5abd9fcfe931732c48e6b2a5eccf1edca4
Amber-Pittman <amber@amberpittman.com>
2019-11-20 21:54:11 -0500

    Commented out Form in FinalContent

--- src/components/FinalPage/FinalContent.js
+++ src/components/FinalPage/FinalContent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Form from './LoginForm'
+// import Form from './LoginForm'
 import Head from './FinalTitle'
 
 const Body=styled.body`
