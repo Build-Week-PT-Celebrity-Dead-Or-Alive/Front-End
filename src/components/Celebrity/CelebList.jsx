0d73e9cddbfcaf1f32bb1be2d97c83f352b1f0ac
Amber-Pittman <amber@amberpittman.com>
2019-11-20 21:48:23 -0500

    Updated AxiosWithAuth path

--- src/components/Celebrity/CelebList.jsx
+++ src/components/Celebrity/CelebList.jsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 //import axios from "axios";
 
-import AxiosWithAuth from "../../../Utilities/AxiosWithAuth";
+import AxiosWithAuth from "../../Utilities/AxiosWithAuth";
 
 const initialState = {
   name: "",
