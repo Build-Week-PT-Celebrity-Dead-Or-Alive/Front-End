34acf61b7608492db95e2c9d150be30ea6a4f513
Danielle B <danibaxx@hotmail.com>
2019-11-21 21:31:16 -0700

    changed up CelebList.js to handle Delete request, created UpdateCeleb.js to handle updating celebs dead/alive, not quite functional but almost there

--- src/App.js
+++ src/App.js
@@ -12,13 +12,14 @@ import SignUp from './components/FinalPage/SignUp/SignUp';
 import Account from './components/FinalPage/Account/Account';
 import Login from './components/FinalPage/Login/Login';
 import Logout from './components/FinalPage/Logout';
-import Admin from './components/Admin/Admin';
 import Users from './components/Admin/Users';
-import './App.css';
-import Final from './components/FinalPage/FinalPage';
-import './Fonts.css';
+import CelebList from './components/Celebrity/CelebList';
+import UpdateCeleb from './components/Celebrity/UpdateCeleb';
+import FinalPage from './components/FinalPage/FinalPage';
 import QuizCard from './components/QuizPage/QuizCard/QuizCard';
 
+import './App.css';
+import './Fonts.css';
 
 class App extends React.Component {
   constructor() {
@@ -74,38 +75,33 @@ class App extends React.Component {
     }
 
     return (
-      <div>
-      <main>
-          <Route exact path="/final" component={Final}/>
-      </main>
       <div className="App">
         <nav className="nav-bar">
-          <Link to='/landingpage'>Home</Link>
+          <Link to='/'>Home</Link>
           {!this.signedIn && <Link to='/signup'>Sign-Up</Link>}
           {this.signedIn && <Link to='/account'>My Account</Link>}
           {!this.signedIn && <Link to='/login'>Login</Link>}
-          {this.signedIn && <Link to='/admin'>Users</Link>}
+          {this.signedIn && <Link to='/users'>Users</Link>}
           {this.signedIn && <Link to='/logout'>Logout</Link>}
         </nav>
-  
-        <Route exact path='/landingpage' component={LandingPage} />
-        <Route exact path='/signup' component={SignUp} />
-        <ProtectedRoute exact path='/account' component={Account} />
-        <Route exact path='/admin' component={Users} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/logout' component={Logout} />
   
         <Route exact path='/' render={(props) =>{
           return(<LandingPage {...props}/>);
         }}/>
+        <Route exact path='/signup' component={SignUp} />
+        <ProtectedRoute exact path='/account' component={Account} />
+        <ProtectedRoute exact path='/users' component={Users} />
+        <Route exact path='/celebs' component={CelebList} />
+        <ProtectedRoute exact path='/celebs/:id' component={UpdateCeleb} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/logout' component={Logout} />
+  
         <Route path="/quizpage">
           {renderQuizCard()}
         </Route>
         <Route path="finalpage">
-  
+
         </Route>
-  
-      </div>
       </div>
     );
   }
