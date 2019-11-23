c013689b1e55ef5f635a2ceffdd6460577a2fffa
Amber-Pittman <amber@amberpittman.com>
2019-11-22 20:35:28 -0500

    Created redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+//import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 
+import { createStore, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux';
+import reducers from './reducers/reducers';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+
+const store = createStore(reducers, applyMiddleware(thunk, logger))
+
 ReactDOM.render(
-  <Router>
+  <Provider store={store}>
     <App />
-  </Router>, 
-  document.getElementById('root')
-);
+  </Provider>, document.getElementById('root'));
